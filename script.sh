d9ac777ed79910b1d26f73d05b0d4d93cd44cc3d
adeepthinarayan <deepthi.narayan@thinknyx.com>
2023-05-12 06:12:14 +0000

    Added a shell script for a simple file execution

---
+++ script.sh
@@ -0,0 +1,11 @@
+echo "***************************Starting the script************************"
+mkdir deepthi
+cd deepthi
+touch file1 > "This is my first file"
+touch file2 > "This is my second file"
+echo "Display files"
+ls
+echo "Display file content"
+cat file1
+cat file2
+echo "****************************************End of script***********************"
